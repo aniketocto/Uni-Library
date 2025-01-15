f65189baca63c3d2ded773c36716ea8b46db12d1
aniketocto <aniketocto@gmail.com>
2025-01-15 12:09:15 +0530

    fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { Button } from "@/components/ui/button";
 
-const Home = () => {
-  return (
-    <>
-      <Button>Click Me</Button>
-    </>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <Button>Click Me</Button>;
+}
